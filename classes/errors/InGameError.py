19c67bed965d42f495579e7d78a6fd3b4566ae2d
Wordsetter0 <86980752+Wordsetter0@users.noreply.github.com>
2021-07-13 15:42:41 +0300

    Delete:Custom Errors
    Scrapped

--- classes/errors/InGameError.py
+++
@@ -1,9 +0,0 @@
-
-
-class InGameError(Exception):
-    """
-    This error is thrown, whenever a player is assigned to a game, when he is already in one
-    """
-    def __init__(self, discord_id: int):
-        self.message = f"Player #{discord_id} is already in a game"
-        super().__init__(self.message)
